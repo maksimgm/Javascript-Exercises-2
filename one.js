0b9360f92cd20cf85427ff0f02b037478c10d879
maksimgm <maksimmamrikov@gmail.com>
2015-07-09 10:50:15 -0700

    complete loop

--- one.js
+++ one.js
@@ -1,6 +1,5 @@
 var dreamCar = {brand:"Lotus", model:"Elise", color:"orange", rims:"chrome", windows:"tinted"};
-var i = 0
-while(i < dreamCar.length) {
-  dreamCar++;
-  console.log(dreamCar);
+
+for(i in dreamCar) {
+  console.log(i,dreamCar[i]);
 }
